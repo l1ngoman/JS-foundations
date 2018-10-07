d8532553eb01670709fb6ee2edf68604d6d1616d
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2018-10-06 23:35:21 -0700

    solved array-methods-practice

--- array-methods-practice.js
+++ array-methods-practice.js
@@ -6,41 +6,57 @@
  // Consider this variable:
  var groceryList1 = ["apples", "carrots", "oatmeal"]
  // Write the code that will add "granola" to the end of array without altering the original array.
+ // var gtemp = groceryList1.concat("granola");
+ // console.log(groceryList1);
+ // console.log(gtemp);
 
  // Exercise 2
  // Consider this variable:
  var groceryList2 = ["chips", "dip", "cookies"]
  // Write the code that will add "soda" to the end of the original array.
+ // groceryList2.push("soda");
+ // console.log(groceryList2);
 
  // Exercise 3
  // Consider this variable:
  var numbers1 = [1, 2, 3, 4, 5]
  // Write the code that will add the number 0 to the beginning of the array without altering the original array.
+ // var ntemp = [0];
+ // ntemp = ntemp.concat(numbers1);
+ // console.log(numbers1);
+ // console.log(ntemp);
 
  // Exercise 4
  // Consider this variable:
  var numbers2 = [2, 4, 6, 8, 10]
  // Write the code that will add the number 0 to the beginning of the original array.
+// numbers2.unshift(0);
+// console.log(numbers2);
 
  // Exercise 5
  // Consider this variable:
  var numbers3 = [2, 13, 6, 8, 4, 2]
  // Write the code that finds the index of the first appearance of the number 2.
-
+// console.log(numbers3.indexOf(2));
  // Exercise 6
  // Write the code that finds the index of the last appearance of the number 2.
+// console.log(numbers3.lastIndexOf(2));
 
  // Exercise 7
  // Consider this variable:
  var chars = ["y", "a", "r", "r", "a"]
  // Write the code that brings all the letters in the chars array together into a string.
+ // console.log(chars.join(''));
 
  // Exercise 8
  // Write the code that reverses the order of the letters in the chars array and saves it into a variable called charsReversed.
+ // var charsReversed = chars.reverse();
+ // console.log(charsReversed);
 
  // Exercise 9
  // Write the code that brings all the letters in the charsReversed array together into a string with a hyphen between each letter.
+ // console.log(charsReversed.join('-'));
 
  // Exercise 10
  // Write the code that brings all the letters in the charsReversed Array together into a string without separators.
-
+// console.log(charsReversed.join(''));

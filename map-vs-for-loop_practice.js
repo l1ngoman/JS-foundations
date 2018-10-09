10448ec60efab26b1242650ae87ad4ac40adcdd0
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2018-10-08 22:26:09 -0700

    completed

--- map-vs-for-loop_practice.js
+++ map-vs-for-loop_practice.js
@@ -45,24 +45,37 @@ var numbers = [11, 22, 33, 44, 55, 66]
 // Consider this variable:
 var toons = [{name: "Tom", animal: "cat"}, {name: "Jerry", animal: "mouse"}, {name: "Bugs", animal: "rabbit"}, {name: "Daffy", animal: "duck"}, {name: "Felix", animal: "cat"}, {name: "Oswald", animal: "rabbit"}]
 // Write a for loop that saves all cat characters to another array.
-console.log(toons.filter(()));
+// console.log(toons.filter(function(toons) {return toons.animal==="cat"}));
+// console.log(toons.filter(toons => toons.animal==="cat"));
 
 // Exercise 6
 // Use map on the toons array to save all rabbit characters to another array.
+// console.log(toons.filter(toons => toons.animal==="rabbit"));
 
 // Exercise 7
 // Write a for loop to save the names of each character to another array.
+// var characters = [];
+// toons.forEach(function(element){
+//   characters.push(element.name);
+// })
+//
+// console.log(characters);
 
 // Exercise 8
 // Use map on the toons array to save a description of each character to another array.
+// console.log(toons.map(toons => toons.description = `${toons.name} the ${toons.animal}`));
 
 // Exercise 9
 // Consider this variable:
 var people = [{firstName: "Alan", lastName: "Grant"}, {firstName: "Ellie", lastName: "Sattler"}, {firstName: "Ian", lastName: "Malcolm"}]
 // Write a for loop that creates a new object for every person that has a key value for firstName, lastName, and fullName and saves each new object into another array.
+// for(let i=0; i<people.length;i++){
+//   people[i].fullName = `${people[i].firstName} ${people[i].lastName}`;
+// }
 
 // Exercise 10
 // Refactor the above code to use a map on the people array to achieve the same result.
+// console.log(people.map(people => people.fullName = `${people.firstName} ${people.lastName}`));
 
 // Exercise 11
 // Consider this function:
@@ -70,6 +83,12 @@ function addTitle(person){
   return "Dr. " + person.firstName + " " + person.lastName
 }
 // Write a for loop that iterates through each person in the people array and stores the result of addTitle on each element to another array.
+// var drs = [];
+// for(let i=0;i<people.length;i++){
+//   drs.push(addTitle(people[i]));
+// }
+// console.log(drs);
 
 // Exercise 12
 // Use map on the run addTitle on each element in the people array and store the result of each call into another array.
+// console.log(people.map(people => addTitle(people)));
